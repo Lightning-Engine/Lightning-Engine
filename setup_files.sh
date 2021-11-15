60ef960ab3875d641fdc61ddd555f27b0b9d46c3
Daan Meijer <daansander02@outlook.com>
2021-11-15 16:40:13 +0100

    Fix setup_files

--- setup_files.sh
+++ setup_files.sh
@@ -1,19 +1,20 @@
 PREMAKE="./Vendor/premake/bin/release/premake5"
+PREMAKE_DIR="./Vendor/premake"
 if [[ -f "$PREMAKE" ]]
 then
 	echo "Premake already exists"
 else
 	if [ "$1" == "osx" ]
 	then
-		make -f Bootstrap.mak osx
+		make -C "$PREMAKE_DIR" -f Bootstrap.mak osx
 	fi
 	if [ "$1" == "linux" ]
 	then
-		make -f Bootstrap.mak linux
+		make -C "$PREMAKE_DIR" -f Bootstrap.mak linux
 	fi
 	if [ "$1" == "win" ]
 	then
-		nmake -f Bootstrap.mak windows
+		nmake -C "$PREMAKE_DIR" -f Bootstrap.mak windows
 	fi
 fi	
 
