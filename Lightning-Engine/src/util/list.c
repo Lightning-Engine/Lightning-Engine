f82f64db46c42c2ea966a1ef224b0151606cff9a
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-02-18 11:09:39 +0100

    Add single linked list (untested)

--- Lightning-Engine/src/util/list.c
+++ Lightning-Engine/src/util/list.c
@@ -1,2 +1,67 @@
 #include "li/list.h"
 
+li_list_t *li_lstnew(void *data) {
+	li_list_t *list;
+
+	list = li_safe_malloc(sizeof(*list));
+	list->next = NULL;
+	list->data = data;
+	return (list);
+}
+
+void li_lstdel(li_list_t *lst) {
+	free(lst);
+}
+
+void li_lstclr(li_list_t *lst) {
+	li_list_t *tmp;
+
+	tmp = lst;
+	while (lst) {
+		tmp = lst->next;
+		li_lstdel(lst);
+		lst = tmp;
+	}
+}
+
+void li_lstadd_back(li_list_t **lst, li_list_t *ele) {
+	if (!*lst)
+		*lst = ele;
+	else {
+		while (*lst) {
+			if (!(*lst)->next)
+				break;
+			*lst = (*lst)->next;
+		}
+		(*lst)->next = ele;
+	}
+}
+
+void li_lstadd_frnt(li_list_t **lst, li_list_t *ele) {
+	ele->next = *lst;
+	*lst = ele;
+}
+
+void li_lstrem(li_list_t **lst, int (*check)(void*,void*), void *param) {
+	while (*lst && check((*lst)->data, param)) {
+		tmp = (*lst)->next;
+		li_lstdel(*lst);
+		*lst = tmp;
+	}
+	while (*lst) {
+		if (check((*lst)->data, param)) {
+			tmp->next = (*lst)->next;
+			li_lstdel(*lst);
+			*lst = temp->next;
+		} else {
+			tmp = *lst;
+			*lst = *lst->next;
+		}
+	}
+}
+
+void li_lstfor(const li_list_t *lst, void (*proc)(void*,void*), void *param) {
+	while (*lst) {
+		proc((*lst)->data, param);
+		*lst = (*lst)->next;
+}
