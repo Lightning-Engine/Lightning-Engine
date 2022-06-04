999054b9b83425ca3572a86fdf54b63cdd973af4
Chen Steenvoorden <dutchen18@gmail.com>
2022-06-04 15:03:59 +0200

    generic main()

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -1,8 +1,14 @@
 #include "sandbox.h"
 
-#include <android/log.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef ANDROID
+# include <android/log.h>
+# define LOG(...) __android_log_print(ANDROID_LOG_INFO, "li", __VA_ARGS__)
+#else
+# define LOG(...) printf(__VA_ARGS__)
+#endif
 
 static int running = 1;
 
@@ -11,11 +17,11 @@ void win_key_fun(
     li_input_key_t key) {
     (void) win;
     if (msg == li_win_msg_key_down) {
-        printf("key_down    %04hx %04hx\n", state, key);
+        LOG("key_down    %04hx %04hx\n", state, key);
     } else if (msg == li_win_msg_key_repeat) {
-        printf("key_repeat  %04hx %04hx\n", state, key);
+        LOG("key_repeat  %04hx %04hx\n", state, key);
     } else {
-        printf("key_up      %04hx %04hx\n", state, key);
+        LOG("key_up      %04hx %04hx\n", state, key);
     }
 }
 
@@ -24,9 +30,9 @@ void win_button_fun(
     li_input_button_t button) {
     (void) win;
     if (msg == li_win_msg_button_down) {
-        printf("button_down %04hx %4d %4d %04hx\n", state, x, y, button);
+        LOG("button_down %04hx %4d %4d %04hx\n", state, x, y, button);
     } else {
-        printf("button_up   %04hx %4d %4d %04hx\n", state, x, y, button);
+        LOG("button_up   %04hx %4d %4d %04hx\n", state, x, y, button);
     }
 }
 
@@ -34,73 +40,63 @@ void win_motion_fun(
     li_win_t win, li_win_msg_t msg, li_input_state_t state, int x, int y) {
     (void) win;
     (void) msg;
-    printf("motion      %04hx %4d %4d\n", state, x, y);
+    LOG("motion      %04hx %4d %4d\n", state, x, y);
 }
 
 void win_size_fun(li_win_t win, li_win_msg_t msg, int width, int height) {
     (void) win;
     (void) msg;
-    printf("size        %4d %4d\n", width, height);
+    LOG("size        %4d %4d\n", width, height);
 }
 
 void win_close_fun(li_win_t win, li_win_msg_t msg) {
     (void) win;
     (void) msg;
-    printf("close\n");
+    LOG("close\n");
     running = 0;
 }
 
 void view_key_fun(
     li_view_msg_t msg, li_input_state_t state, li_input_key_t key) {
     if (msg == li_view_msg_key_down) {
-        __android_log_print(
-            ANDROID_LOG_INFO, "li", "key_down    %04hx %04hx", state, key);
+        LOG("key_down    %04hx %04hx", state, key);
     } else if (msg == li_view_msg_key_repeat) {
-        __android_log_print(
-            ANDROID_LOG_INFO, "li", "key_repeat  %04hx %04hx", state, key);
+        LOG("key_repeat  %04hx %04hx", state, key);
     } else if (msg == li_view_msg_key_up) {
-        __android_log_print(
-            ANDROID_LOG_INFO, "li", "key_up      %04hx %04hx", state, key);
+        LOG("key_up      %04hx %04hx", state, key);
     }
 }
 
 void view_touch_fun(
     li_view_msg_t msg, li_input_state_t state, int x, int y, int id) {
     if (msg == li_view_msg_touch_down) {
-        __android_log_print(
-            ANDROID_LOG_INFO, "li", "touch_down  %04hx %4d %4d %4d", state, x,
-            y, id);
+        LOG("touch_down  %04hx %4d %4d %4d", state, x, y, id);
     } else if (msg == li_view_msg_touch_up) {
-        __android_log_print(
-            ANDROID_LOG_INFO, "li", "touch_up    %04hx %4d %4d %4d", state, x,
-            y, id);
+        LOG("touch_up    %04hx %4d %4d %4d", state, x, y, id);
     } else {
-        __android_log_print(
-            ANDROID_LOG_INFO, "li", "touch_move  %04hx %4d %4d %4d", state, x,
-            y, id);
+        LOG("touch_move  %04hx %4d %4d %4d", state, x, y, id);
     }
 }
 
 void li_main(void) {
-    /*
     li_win_t win;
     li_win_key_fun    = win_key_fun;
     li_win_button_fun = win_button_fun;
     li_win_motion_fun = win_motion_fun;
     li_win_size_fun   = win_size_fun;
     li_win_close_fun  = win_close_fun;
-    li_win_init();
-    win = li_win_create(640, 480);
-    while (running) {
-        li_win_poll();
-    }
-    li_win_destroy(win);
-    li_win_exit();
-    */
     li_view_key_fun   = view_key_fun;
     li_view_touch_fun = view_touch_fun;
-    li_view_init();
-    printf("li_main\n");
-    while (1)
-        li_view_poll();
+    LOG("li_main\n");
+    if (li_win_init() == 0) {
+        win = li_win_create(640, 480);
+        while (running) {
+            li_win_poll();
+        }
+        li_win_destroy(win);
+        li_win_exit();
+    } else if (li_view_init() == 0) {
+        while (1)
+            li_view_poll();
+    }
 }
